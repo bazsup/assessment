ec5eb3fd49ad03177323a9b2e9b5d0622ddde2ec
bazsup <bas@odds.team>
2023-01-03 13:46:18 +0700

    add create expense handler

---
+++ expense/create_test.go
@@ -0,0 +1,55 @@
+//go:build unit
+
+package expense_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/bazsup/assessment/expense"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+
+	_ "github.com/lib/pq"
+)
+
+// TODO: remove dependency
+func init() {
+	expense.InitDB()
+}
+
+func TestCreateExpense(t *testing.T) {
+	// Arrange
+	// TODO: grouping this
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/expenses", bytes.NewBufferString(`{
+		"title": "test-title",
+		"amount": 39000,
+		"note": "test-note",
+		"tags": ["test-tag1", "test-tag2"]
+	}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+
+	// Act
+	err := expense.CreateExpenseHandler(c)
+
+	var exp expense.Expense
+	json.NewDecoder(rec.Body).Decode(&exp)
+
+	// Assertions
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+
+		assert.NotEqual(t, 0, exp.ID)
+		assert.Equal(t, "test-title", exp.Title)
+		assert.Equal(t, float64(39000), exp.Amount)
+		assert.Equal(t, "test-note", exp.Note)
+		assert.Equal(t, []string{"test-tag1", "test-tag2"}, exp.Tags)
+	}
+}
